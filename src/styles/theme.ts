1d417ef89f9f1d11104206d8c26ed084fd7f83c6
Yuri <yuriqpaiva@gmail.com>
2022-07-17 06:37:51 -0300

    Project setup using chakra

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme, Theme, DeepPartial } from "@chakra-ui/react";
+
+const customTheme = {
+  styles: {
+    global: {
+      body: {
+        bg: "gray.100",
+        color: "gray.400",
+      },
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  colors: {
+    gray: {
+      "100": "#F5F8FA",
+      "400": "#47585B",
+    },
+  },
+} as DeepPartial<Theme>;
+
+export const theme = extendTheme(customTheme);
