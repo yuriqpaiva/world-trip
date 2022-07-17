e4f002ee61cb55c286e28cdb0631e4d9cffe0a9f
Yuri <yuriqpaiva@gmail.com>
2022-07-17 17:42:45 -0300

    Get continent info at json server

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -1,3 +1,53 @@
-export default function Continent() {
+import { GetStaticPaths, GetStaticProps } from "next";
+
+type ContinentData = {
+  id: number;
+  name: string;
+  description: string;
+  info: {
+    countries: number;
+    languages: number;
+    cities: number;
+  };
+  slug: string;
+};
+
+interface ContinentProps {
+  continent: ContinentData;
+}
+
+export default function Continent({ continent }: ContinentProps) {
   return <h1>Continent</h1>;
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch("http://localhost:3333/continents");
+  const data: ContinentData[] = await response.json();
+
+  return {
+    paths: data.map((continent) => {
+      return {
+        params: {
+          continent: continent.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const continent = params?.continent;
+
+  const response = await fetch(
+    `http://localhost:3333/continents/?slug=${continent}`
+  );
+
+  const data: ContinentData = await response.json();
+
+  return {
+    props: {
+      continent: data,
+    },
+  };
+};
